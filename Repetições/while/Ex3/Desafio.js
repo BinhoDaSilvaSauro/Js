bf1ec0c11f0402265a64e854896f9f365713ccff
Rubens <rubenslira371@gmail.com>
2022-09-17 14:54:15 -0300

    Segunda Versão

--- Repetições/while/Ex3/Desafio.js
+++ Repetições/while/Ex3/Desafio.js
@@ -1,7 +1,9 @@
-var CTxt = document.querySelector("#P")
-let MenorValor
-let MaiorValor
-let ValorTotal
+let CTxt = document.querySelector("#P")
+let Tabela = document.querySelector("#Tabela")
+let R = document.querySelector("#Resultado")
+let MenorValor = 100
+let MaiorValor = 0
+let ValorTotal = 0
 let Elementos = []
 function Adicionar() {
     let Valor = Number(CTxt.value)
@@ -10,18 +12,32 @@ function Adicionar() {
     } else {
         Elementos.push(Valor)
         ValorTotal += Valor
-        window.alert("Adicionado")
-        window.alert(Elementos.indexOf(Valor))
+        let item = document.createElement("option")
+        item.text = `Valor ${Valor} adicionado`
+        Tabela.appendChild(item)
         if (Valor < MenorValor) {
             MenorValor = Valor
         }
         if (Valor > MaiorValor) {
             MaiorValor = Valor
         }
+        R.innerHTML = ``
     }
 }
 function Verificar() {
-    window.alert(`O valor do setor é ${Elementos}`)  
+    if (Elementos.length == 0) {
+        window.alert("Adicione Valores")
+    } else {
+        if (Elementos.length == 1) {
+            R.innerHTML = `<p>Ao todo temos <strong>${Elementos.length}</strong> valor cadastrado</p>`
+        } else {
+            R.innerHTML = `<p>Ao todo temos <strong>${Elementos.length}</strong> valores cadastrados</p>`
+        }
+        R.innerHTML += `<p>O maior valor foi <strong>${MaiorValor}</strong></p>`
+        R.innerHTML += `<p>O menor valor foi <strong>${MenorValor}</strong></p>`
+        R.innerHTML += `<p>A média de total todos os elementos foi <strong>${ValorTotal/Elementos.length}</strong></p>`
+        R.innerHTML += `<p>A soma total dos elementos foi <strong>${ValorTotal}</strong></p>`
+    }
 }
 
 function Limpar() {
@@ -29,11 +45,7 @@ function Limpar() {
     MenorValor = 0
     MaiorValor = 0
     ValorTotal = 0
+    Tabela.innerHTML = "<option>Os números aparecem aqui</option>"
     window.alert(`O setor foi limpo ${Elementos}`)
+    R.innerHTML = ``
 }
-
-/*
-    Faltam as opções
-    Limpar as opções
-    Exibição final
-*/
